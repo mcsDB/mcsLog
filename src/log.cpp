cd75998702627b4bf7db1ea674070c6907dbb3c9
SoujanyaPonnapalli <soujanyap95@gmail.com>
2020-02-05 21:05:17 -0600

    :boom: Basic structure for a log

---
+++ src/log.cpp
@@ -0,0 +1,53 @@
+#define LOGSIZE 128*1024*1024
+
+#include <fcntl.h>
+#include <stdexcept>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "log.hpp"
+
+namespace mcsLog {
+
+    Logger::Logger(const char* path) {
+        _logfile_path = path;
+        // Sets _logfile_fd, _logfile_size, and _logfile_offset
+        recover();
+        _logfile_mmap_addr = mmap(NULL, _logfile_size, PROT_READ | PROT_WRITE,
+         MAP_PRIVATE | MAP_POPULATE, _logfile_fd, 0);
+        if (_logfile_mmap_addr == 0) {
+            throw std::runtime_error("Error: Could not mmap the logfile");
+        }
+    }
+
+    Logger::~Logger() {
+        munmap(_logfile_mmap_addr, _logfile_size);
+    }
+
+    void Logger::recover() {
+        _logfile_fd = open(_logfile_path, O_RDWR, 0666);
+        if (_logfile_fd < 0) {
+            _logfile_offset = 0;
+            _logfile_fd = open(_logfile_path, O_RDWR | O_CREAT, 0666);
+            if (_logfile_fd < 0)
+                throw std::runtime_error("Error: Could not open the logfile");
+            // [TODO]: Make this environment independent?
+            int falloc_ret = posix_fallocate(_logfile_fd, 0, LOGSIZE);
+            _logfile_size = LOGSIZE;
+        }
+        else {
+            // [TODO]: Implement recovery on the existing logfile
+            // For now, rewriting the logfile - setting the offset to 0
+            _logfile_offset = 0;
+            _logfile_size = LOGSIZE;
+        }
+    }
+
+    unsigned long long Logger::Write(void* value, int length) {        
+        long long write_offset = __sync_fetch_and_add(&_logfile_offset, length);
+        // [TODO]: If write_offset is beyond resize_threshold, extend the file size
+        memcpy(_logfile_mmap_addr + write_offset, value, length);
+    }
+
+} // namespace mcsLog
---
+++ src/log.hpp
@@ -0,0 +1,15 @@
+namespace mcsLog {
+    class Logger {
+    private:
+        int _logfile_fd;
+        const char* _logfile_path;
+        long long _logfile_offset;
+        long long _logfile_size;
+        void* _logfile_mmap_addr;
+
+        void recover();
+    public:
+        Logger(const char* path);
+        unsigned long long Write(void* value, int length);
+    };
+} // namespace Logger
